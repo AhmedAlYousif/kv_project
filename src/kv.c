cdef9e34d5cf7a989e2cbb07137100f6c4e5a615
Ahmed Al Yousif <ahmadalsiddeeq@gmail.com>
2026-05-04 23:42:44 +0300

    return 0

--- src/kv.c
+++ src/kv.c
@@ -63,7 +63,7 @@ int kv_put(kv_t *db, char *key, char *value) {
       if (!newval)
         return -1;
       entry->value = newval;
-      return real_idx;
+      return 0;
     }
     if (!entry->key || entry->key == (void *)TOMBSTONE) {
       char *newval = strdup(value);
@@ -76,7 +76,7 @@ int kv_put(kv_t *db, char *key, char *value) {
       entry->value = newval;
       entry->key = newkey;
       db->count++;
-      return real_idx;
+      return 0;
     }
   }
 
